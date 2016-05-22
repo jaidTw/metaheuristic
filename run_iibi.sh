82b535cccb572f6b8b7cdc43b2ef7b765f773109
AIGecko <aigecko@hotmail.com>
2016-05-23 01:33:44 +0800

    update runner

---
+++ run_iibi.sh
@@ -0,0 +1,11 @@
+./pfsp_iibi_1k3h_calc tai20_5_1.txt >> tai20_5_iibi_1k3h_calc_op.result
+./pfsp_iibi_1k3h_calc tai20_10_1.txt >> tai20_10_iibi_1k3h_calc_op.result
+./pfsp_iibi_1k3h_calc tai20_20_1.txt >> tai20_20_iibi_1k3h_calc_op.result
+
+./pfsp_iibi_1k3h_calc tai50_5_1.txt >> tai50_5_iibi_1k3h_calc_op.result
+./pfsp_iibi_1k3h_calc tai50_10_1.txt >> tai50_10_iibi_1k3h_calc_op.result
+./pfsp_iibi_1k3h_calc tai50_20_1.txt >> tai50_20_iibi_1k3h_calc_op.result
+
+./pfsp_iibi_1k3h_calc tai100_5_1.txt >> tai100_5_iibi_1k3h_calc_op.result
+./pfsp_iibi_1k3h_calc tai100_10_1.txt >> tai100_10_iibi_1k3h_calc_op.result
+./pfsp_iibi_1k3h_calc tai100_20_1.txt >> tai100_20_iibi_1k3h_calc_op.result
